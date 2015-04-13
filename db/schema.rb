1e9c4d0d3dd2046144b5b889738c6ec23266fabc
JimiJonJimbo <jimmyling3@gmail.com>
2015-04-12 21:24:47 -0700

    Get rid of links table

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20150301020603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "links", force: true do |t|
-    t.string   "title"
-    t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
