a0e752bd0f98f25e293772222024864792c46a17
Jimmy Ling <jimmyling3@gmail.com>
2015-06-11 22:55:23 -0700

    Get thumbnails working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,27 @@
+require 'embedly'
+require 'json'
+
 class Post < ActiveRecord::Base
   acts_as_votable
 
   belongs_to :user
 
   validates :link, url: true
+
+  def thumbnail
+    embedly_api = Embedly::API.new :key => '7538ada32412465384e838619f4fc31e',
+        :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
+
+    obj = embedly_api.extract url: link
+    o = obj.first
+    image = o.images.first
+
+    if image.present?
+      return image['url']
+    else
+      return nil
+    end
+  end
 
   def score
     s = votes_for.count
