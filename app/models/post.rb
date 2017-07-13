ce8efbb888cf0247dc73c028d8023b93ee0b1d4b
JimiJonJimbo <jimmyling3@gmail.com>
2017-07-13 12:44:03 -0700

    Revert "Get link_thumbnailer working"
    This reverts commit 04b784893dd718a60706992c569132b028c53d0a.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-require 'link_thumbnailer'
-
 class Post < ActiveRecord::Base
   acts_as_votable
 
@@ -8,7 +6,7 @@ class Post < ActiveRecord::Base
   validates :link, url: true
 
   def thumbnail
-    LinkThumbnailer.generate(link).images.first || link
+    return nil
   end
 
   def score
