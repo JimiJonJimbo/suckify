3778aba247d4df0f3bf7b79640251d1f613362bf
JimiJonJimbo <jimmyling3@gmail.com>
2017-07-28 20:37:20 -0700

    Break the tile layout and remove "This sucks"

--- spec/features/posts/user_votes_on_post_spec.rb
+++ spec/features/posts/user_votes_on_post_spec.rb
@@ -7,7 +7,7 @@ feature 'User votes on post' do
   context 'when not logged in' do
     scenario 'must log in to vote' do
       visit post_path(post)
-      click_on 'This sucks'
+      find('.vote').click
       expect(page).to have_content 'You must log in to vote.'
       expect(page).to have_content 'Log in'
     end
@@ -21,7 +21,7 @@ feature 'User votes on post' do
     scenario 'can vote' do
       visit post_path(post)
       expect(page).to have_content '0 votes'
-      click_on 'This sucks'
+      find('.vote').click
       expect(page).to have_content 'Voted'
       expect(page).to have_content '1 vote'
     end
